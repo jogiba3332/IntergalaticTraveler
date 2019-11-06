5f894b853b36932940e98f72c3896f83f71b600d
Josh Ogiba <jogiba3332@stu.d214.org>
2019-11-06 11:31:31 -0600

    comment added

---
+++ IntergalaticTraveler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalaticTraveler
+//
+//  Created by Josh Ogiba on 11/6/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+// This is cool
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
