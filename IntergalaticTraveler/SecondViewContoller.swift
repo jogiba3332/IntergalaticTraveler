e609407fd90d4520b6b25dc0f2af2ace7be6f4f2
Josh Ogiba <jogiba3332@stu.d214.org>
2019-11-11 11:21:53 -0600

    changes

---
+++ IntergalaticTraveler/SecondViewContoller.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewContoller.swift
+//  IntergalaticTraveler
+//
+//  Created by Josh Ogiba on 11/11/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewContoller: UIViewController {
+    @IBOutlet weak var starImageView: UIImageView!
+    var starcolor: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let randomNumber = Int.random(in: 1...3)
+        let myImage = UIImage(named: "\(starcolor)\(randomNumber)")
+        
+        starImageView.image = myImage
+    }
+    
+
+ 
+
+}
